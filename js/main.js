b1b9719a88f846ea283cac64376df4c089e609dd
tusharprajapati <mr.tusharprajapati@gmail.com>
2022-06-26 18:13:14 +0530

    new design

--- js/main.js
+++ js/main.js
@@ -1,33 +1,22 @@
-// Select DOM Items
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
-
-// Set Initial State Of Menu
-let showMenu = false;
-
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show'));
-
-    // Set Menu State
-    showMenu = true;
-  } else {
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
-
-    // Set Menu State
-    showMenu = false;
-  }
-}
+var granimInstance = new Granim({
+    element: '#canvas-image-blending',
+    direction: 'radial',
+    isPausedWhenNotInView: true,
+    image: {
+        source: './img/trees.jpg',
+        blendingMode: 'multiply',
+        stretchMode: ['stretch', 'stretch']
+    },
+    states: {
+        "default-state": {
+            gradients: [
+                ['#000000', '#ffffff'],
+                ['#29323c', '#485563'],
+                ['#FF6B6B', '#556270'],
+                ['#5f3d08', '#896937'],
+                ['#38761d', '#82af6f']
+            ],
+            transitionSpeed: 7000
+        }
+    }
+});
